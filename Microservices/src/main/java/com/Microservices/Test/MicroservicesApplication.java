4b94b5fb6dd454a797f757f0990cfe134da68d49
vilas-1234 <vilaspokarne97@gmail.com>
2024-03-23 17:25:45 +0530

    my second commit

--- Microservices/src/main/java/com/Microservices/Test/MicroservicesApplication.java
+++ Microservices/src/main/java/com/Microservices/Test/MicroservicesApplication.java
@@ -1,5 +1,5 @@
 package com.Microservices.Test;
-
+import java.util.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -11,6 +11,8 @@ public class MicroservicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroservicesApplication.class, args);
+
+List<Integer> list=Arrays.asList(1,2,3);
 	}
 
 }
