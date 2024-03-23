373af4b2066e062c65bd706485a47bc8e41c4c2b
vilaspokarne1211 <164625758+vilaspokarne1211@users.noreply.github.com>
2024-03-23 17:31:11 +0530

    Update MicroservicesApplication.java

--- Microservices/src/main/java/com/Microservices/Test/MicroservicesApplication.java
+++ Microservices/src/main/java/com/Microservices/Test/MicroservicesApplication.java
@@ -1,5 +1,5 @@
 package com.Microservices.Test;
-
+VIlas manik pokarne
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
